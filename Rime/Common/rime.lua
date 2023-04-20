87b4b2ae132fb487f26a70a93e798d31ac31eb5f
WithdewHua <i@10101.io>
2023-04-20 19:56:52 +0800

    fix(lua): 同步 https://github.com/iDvel/rime-ice/commit/87940fd97e84304719fa23ab04f93f69e7d768bd

--- Rime/Common/rime.lua
+++ Rime/Common/rime.lua
@@ -62,6 +62,16 @@ function date_translator(input, seg, env)
         cand.quality = 100
         yield(cand)
     end
+    -- -- 输出内存
+    -- local cand = Candidate("date", seg.start, seg._end, ("%.f"):format(collectgarbage('count')), "")
+    -- cand.quality = 100
+    -- yield(cand)
+    -- if input == "xxx" then
+    --     collectgarbage()
+    --     local cand = Candidate("date", seg.start, seg._end, "collectgarbage()", "")
+    --     cand.quality = 100
+    --     yield(cand)
+    -- end
 end
 -------------------------------------------------------------
 -- 以词定字
@@ -155,25 +165,48 @@ function long_word_filter(input, env)
     local count = config:get_int(env.name_space .. "/count") or 2
     local idx = config:get_int(env.name_space .. "/idx") or 4
 
-    local l = {}
-    local firstWordLength = 0 -- 记录第一个候选词的长度，提前的候选词至少要比第一个候选词长
-    local s = 0 -- 记录筛选了多少个词条(只提升 count 个词的权重)
-
-    local i = 1
-    for cand in input:iter() do
-        local leng = utf8.len(cand.text)
-        if (firstWordLength < 1 or i < idx) then
-            i = i + 1
-            firstWordLength = leng
+    local code = env.engine.context.input -- 当前编码
+    if string.find(code, "[aeo]") then    -- 要提升的词汇的拼音一定是包含 a o e 的
+        local l = {}
+        local firstWordLength = 0 -- 记录第一个候选词的长度，提前的候选词至少要比第一个候选词长
+        local done = 0 -- 记录筛选了多少个词条(只提升 count 个词的权重)
+        local i = 1
+        for cand in input:iter() do
+            -- 找到要提升的词
+            local leng = utf8.len(cand.text)
+            if (firstWordLength < 1 or i < idx) then
+                i = i + 1
+                firstWordLength = leng
+                yield(cand)
+            elseif ((leng > firstWordLength) and (done < count)) and (string.find(cand.text, "[%w%p%s]+") == nil) then
+                yield(cand)
+                done = done + 1
+            else
+                table.insert(l, cand)
+            end
+            -- 找齐了或者 l 太大了，就不找了
+            if (done == count) or (#l > 50) then
+                break
+            end
+        end
+		-- yield l
+        for _, cand in ipairs(l) do
             yield(cand)
-        elseif ((leng > firstWordLength) and (s < count)) and (string.find(cand.text, "[%w%p%s]+") == nil) then
-            yield(cand)
-            s = s + 1
+        end
+		-- l 弄完了立马给丫回收了
+		l = nil
+        if collectgarbage('count') < 3000 then
+            collectgarbage("step")
         else
-            table.insert(l, cand)
+            collectgarbage('collect')
+        end
+		-- yield 其他
+        for cand in input:iter() do
+            yield(cand)
         end
     end
-    for _, cand in ipairs(l) do
+
+    for cand in input:iter() do
         yield(cand)
     end
 end
