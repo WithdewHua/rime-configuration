2d7e59eaec03090b8c5924f0cc181452cb4b6066
WithdewHua <i@10101.io>
2023-05-05 02:18:38 +0800

    refactor: rename script

--- Rime/Common/lua/is_in_user_dict.lua
+++ Rime/Common/lua/is_in_user_dict.lua
@@ -1,10 +1,10 @@
 -- 这是用户词吗？
 -- 非常简单的lua滤镜，为所有用户词都加上*作为提示
-local function is_that_a_user_dict(input,env)
+local function is_in_user_dict(input,env)
     for cand in input:iter() do
         if(string.find(cand.type,"user"))then cand.comment=cand.comment..'*'end
         yield(cand)
     end 
 end
 
-return is_that_a_user_dict
+return is_in_user_dict
