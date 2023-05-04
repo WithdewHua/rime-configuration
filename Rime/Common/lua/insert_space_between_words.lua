2c228d72c2733784c596ec7e7f052427a025abf2
WithdewHua <i@10101.io>
2023-05-05 01:43:09 +0800

    fix(lua): 修复 lua 脚本，候选词中中英文间自动插入空格

--- Rime/Common/lua/insert_space_between_words.lua
+++ Rime/Common/lua/insert_space_between_words.lua
@@ -4,13 +4,13 @@ local function insert_space_between_words(input, env)
     for cand in input:iter() do
         -- input:inter() 中 cand 为 Phrase，无法修改 cand.text
         -- 在英文或者数字字符前后之间插入空格
-        local text
-        text = cand.text:gsub("([%a%d]+)", " %1 ")
+        local text = cand.text
+        text = text:gsub("([%a%d]+)", " %1 ")
         -- 去除候选词首尾空格
-        text = cand.text:gsub("^%s*(.-)%s*$", "%1")
-        log.info(text)
+        text = text:gsub("^%s*(.-)%s*$", "%1")
+        -- 多个空格替换为一个空格
+        text = text:gsub("%s%s+", " ")
         cand = cand:to_shadow_candidate(cand.type, text, cand.comment)
-        log.info(cand.text)
         yield(cand)
     end 
 end
