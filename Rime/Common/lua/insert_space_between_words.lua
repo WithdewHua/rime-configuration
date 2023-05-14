2df68ac61c85447e804c6bc04bc022e86eee2ec2
WithdewHua <i@10101.io>
2023-05-14 23:53:05 +0800

    fix: 自动插入空格增加条件

--- Rime/Common/lua/insert_space_between_words.lua
+++ Rime/Common/lua/insert_space_between_words.lua
@@ -1,16 +1,19 @@
 -- 候选词中英文字符间插入空格
-
+local utf8 = require("utf8")
 local function insert_space_between_words(input, env)
     for cand in input:iter() do
         -- input:inter() 中 cand 为 Phrase，无法修改 cand.text
-        -- 在英文或者数字字符前后之间插入空格
         local text = cand.text
-        text = text:gsub("([%a%d]+)", " %1 ")
-        -- 去除候选词首尾空格
-        text = text:gsub("^%s*(.-)%s*$", "%1")
-        -- 多个空格替换为一个空格
-        text = text:gsub("%s%s+", " ")
-        cand = cand:to_shadow_candidate(cand.type, text, cand.comment)
+        -- 含有非 ASCII 码字符时才做处理
+        if utf8.len(text) ~= #text then
+            -- 在英文或者数字字符前后之间插入空格
+            text = text:gsub("([%a%d]+)", " %1 ")
+            -- 去除候选词首尾空格
+            text = text:gsub("^%s*(.-)%s*$", "%1")
+            -- 多个空格替换为一个空格
+            text = text:gsub("%s%s+", " ")
+            cand = cand:to_shadow_candidate(cand.type, text, cand.comment)
+        end
         yield(cand)
     end 
 end
