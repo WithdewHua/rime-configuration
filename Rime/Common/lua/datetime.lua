d8de0c8b8cc3d42c8888aeb2a8b64f16a82784ca
WithdewHua <i@10101.io>
2023-05-14 23:52:19 +0800

    refactor: sync https://github.com/iDvel/rime-ice/pull/265

--- Rime/Common/lua/datetime.lua
+++ Rime/Common/lua/datetime.lua
@@ -1,66 +1,83 @@
 -- 日期时间
 -- 提高权重的原因：因为在方案中设置了大于 1 的 initial_quality，导致 rq sj xq dt ts 产出的候选项在所有词语的最后。
+local formats = {
+    date = {
+        '%Y-%m-%d',
+        '%Y/%m/%d',
+        '%Y.%m.%d',
+        '%Y%m%d',
+        '%Y 年 ' .. tostring(tonumber(os.date('%m'))) .. ' 月 ' .. tostring(tonumber(os.date('%d'))) .. ' 日'
+    },
+    time = {
+        '%H:%M',
+        '%H:%M:%S'
+    },
+    datetime = {
+        '%Y-%m-%dT%H:%M:%S+08:00',
+        '%Y%m%d%H%M%S'
+    },
+    week = {
+        '星期%s',
+        '礼拜%s',
+        '周%s'
+    },
+    timestamp = {
+        '%d'
+    }
+}
+
 local function date_translator(input, seg, env)
-    local config = env.engine.schema.config
-    local date = config:get_string(env.name_space .. "/date") or "rq"
-    local time = config:get_string(env.name_space .. "/time") or "sj"
-    local week = config:get_string(env.name_space .. "/week") or "xq"
-    local datetime = config:get_string(env.name_space .. "/datetime") or "dt"
-    local timestamp = config:get_string(env.name_space .. "/timestamp") or "ts"
+    if not env.date then
+        local config = env.engine.schema.config
+        env.name_space = env.name_space:gsub('^*', '')
+        env.date = config:get_string(env.name_space .. '/date') or 'rq'
+        env.time = config:get_string(env.name_space .. '/time') or 'sj'
+        env.week = config:get_string(env.name_space .. '/week') or 'xq'
+        env.datetime = config:get_string(env.name_space .. '/datetime') or 'dt'
+        env.timestamp = config:get_string(env.name_space .. '/timestamp') or 'ts'
+    end
+
+    local current_time = os.time()
+
+    local yield_cand = function(type, text)
+        local cand = Candidate(type, seg.start, seg._end, text, '')
+        cand.quality = 100
+        yield(cand)
+    end
+
     -- 日期
-    if (input == date) then
-        local cand = Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), "")
-        cand.quality = 100
-        yield(cand)
-        local cand = Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), "")
-        cand.quality = 100
-        yield(cand)
-        local cand = Candidate("date", seg.start, seg._end, os.date("%Y.%m.%d"), "")
-        cand.quality = 100
-        yield(cand)
-        local cand = Candidate("date", seg.start, seg._end,
-            os.date("%Y 年 ") .. tostring(tonumber(os.date("%m"))) .. os.date(" 月 %d 日"), "")
-        cand.quality = 100
-        yield(cand)
+    if (input == env.date) then
+        for _, fmt in ipairs(formats.date) do
+            yield_cand('date', os.date(fmt, current_time))
+        end
     end
     -- 时间
-    if (input == time) then
-        local cand = Candidate("time", seg.start, seg._end, os.date("%H:%M"), "")
-        cand.quality = 100
-        yield(cand)
-        local cand = Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), "")
-        cand.quality = 100
-        yield(cand)
+    if (input == env.time) then
+        for _, fmt in ipairs(formats.time) do
+          yield_cand('time', os.date(fmt, current_time))
+        end
     end
     -- 星期
-    if (input == week) then
-        local weakTab = {'日', '一', '二', '三', '四', '五', '六'}
-        local cand = Candidate("week", seg.start, seg._end, "星期" .. weakTab[tonumber(os.date("%w") + 1)], "")
-        cand.quality = 100
-        yield(cand)
-        local cand = Candidate("week", seg.start, seg._end, "礼拜" .. weakTab[tonumber(os.date("%w") + 1)], "")
-        cand.quality = 100
-        yield(cand)
-        local cand = Candidate("week", seg.start, seg._end, "周" .. weakTab[tonumber(os.date("%w") + 1)], "")
-        cand.quality = 100
-        yield(cand)
+    if (input == env.week) then
+        local week_tab = { '日', '一', '二', '三', '四', '五', '六' }
+        for _, fmt in ipairs(formats.week) do
+          local text = week_tab[tonumber(os.date('%w', current_time) + 1)]
+          yield_cand('week', string.format(fmt, text))
+        end
     end
     -- ISO 8601/RFC 3339 的时间格式 （固定东八区）（示例 2022-01-07T20:42:51+08:00）
-    if (input == datetime) then
-        local cand = Candidate("datetime", seg.start, seg._end, os.date("%Y-%m-%dT%H:%M:%S+08:00"), "")
-        cand.quality = 100
-        yield(cand)
-        local cand = Candidate("time", seg.start, seg._end, os.date("%Y%m%d%H%M%S"), "")
-        cand.quality = 100
-        yield(cand)
+    if (input == env.datetime) then
+        for _, fmt in ipairs(formats.datetime) do
+          yield_cand('datetime', os.date(fmt, current_time))
+        end
     end
     -- 时间戳（十位数，到秒，示例 1650861664）
-    if (input == timestamp) then
-        local cand = Candidate("datetime", seg.start, seg._end, os.time(), "")
-        cand.quality = 100
-        yield(cand)
+    if (input == env.timestamp) then
+        for _, fmt in ipairs(formats.timestamp) do
+          yield_cand('timestamp', string.format(fmt, current_time))
+        end
     end
-    -- -- 输出内存
+    -- -- 显示内存
     -- local cand = Candidate("date", seg.start, seg._end, ("%.f"):format(collectgarbage('count')), "")
     -- cand.quality = 100
     -- yield(cand)
