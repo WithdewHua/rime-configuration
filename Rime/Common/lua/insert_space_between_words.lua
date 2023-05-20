f567f3336561720ed2404a594ef03749cd796e5e
WithdewHua <i@10101.io>
2023-05-20 16:35:23 +0800

    fix: 自动加入空格导致部分 emoji 显示异常

--- Rime/Common/lua/insert_space_between_words.lua
+++ Rime/Common/lua/insert_space_between_words.lua
@@ -1,11 +1,28 @@
 -- 候选词中英文字符间插入空格
 local utf8 = require("utf8")
+
+local function has_mixed_chars(str)
+    local has_cn = false
+    local has_asc = false
+    for _, char in utf8.codes(str) do
+        if char >= 19968 and char <= 40869 then
+            has_cn = true
+        elseif char >= 0 and char <= 127 then
+            has_asc = true
+        end
+        if has_cn and has_asc then
+            return true
+        end
+    end
+    return false
+end
+
 local function insert_space_between_words(input, env)
     for cand in input:iter() do
         -- input:inter() 中 cand 为 Phrase，无法修改 cand.text
         local text = cand.text
-        -- 含有非 ASCII 码字符时才做处理
-        if utf8.len(text) ~= #text then
+        -- 含有中文及 ASCII 字符时才做处理
+        if has_mixed_chars(text) then
             -- 在英文或者数字字符前后之间插入空格
             text = text:gsub("([%a%d]+)", " %1 ")
             -- 去除候选词首尾空格
