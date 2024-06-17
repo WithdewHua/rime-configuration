23227a594dd2717824a5a6a271224de5a89b8f3c
WithdewHua <i@10101.io>
2024-06-18 03:05:57 +0800

    feat: 修改 moran 方案，适配全拼词库

--- Rime/Common/lua/is_in_user_dict.lua
+++ Rime/Common/lua/is_in_user_dict.lua
@@ -1,7 +1,7 @@
 -- 若候选词为用户词，则加上 * 作为提示
 local function is_in_user_dict(input,env)
     for cand in input:iter() do
-        if(string.find(cand.type,"user"))then cand.comment=cand.comment..'*'end
+        if(string.find(cand.type,"user"))then cand.comment=cand.comment..' ☯'end
         yield(cand)
     end 
 end
