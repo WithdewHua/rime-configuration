3ca2acae450d96eb9c76bde1a049078ca179bc5f
WithdewHua <i@10101.io>
2024-06-18 03:59:26 +0800

    sync: 同步词库

--- scripts/update_upstream_dicts.py
+++ scripts/update_upstream_dicts.py
@@ -17,21 +17,28 @@ for item in file_list:
     owner = item['owner']
     repo = item['repo']
     branch = item['branch']
-    files = item['files']
+    downloads = item['downloads']
     
     # 循环下载文件并保存到本地
-    for file in files:
-        # 构造 请求 URL
-        url = f'https://github.com/{owner}/{repo}/raw/{branch}/{file["path"]}'
-    
-        # 发送 请求
-        response = requests.get(url)
+    for download in downloads:
+        remote_path = download["path"].rstrip("/")
+        local_path = download["local_path"].rstrip("/")
+        files = download["files"]
+        for file in files:
+            # 构造 请求 URL
+            if remote_path != ".":
+                url = f'https://github.com/{owner}/{repo}/raw/{branch}/{remote_path}/{file}'
+            else:
+                url = f'https://github.com/{owner}/{repo}/raw/{branch}/{file}'
+        
+            # 发送 请求
+            response = requests.get(url)
 
-        # 获取文件内容并保存到本地文件
-        if response.status_code == 200:
-            print(f"Downloaded {url} to {os.path.join(local_path_prefix, file['local_path'])}")
-            with open(os.path.join(local_path_prefix, file['local_path']), 'wb') as f:
-                f.write(response.content)
-        else:
-            print(f"Fail to download {url}")
+            # 获取文件内容并保存到本地文件
+            if response.status_code == 200:
+                print(f"Downloaded {url} to {os.path.join(local_path_prefix, local_path, file)}")
+                with open(os.path.join(local_path_prefix, local_path, file), 'wb') as f:
+                    f.write(response.content)
+            else:
+                print(f"Fail to download {url}")
 
